08ca792d8c3439a0544ddf6a7b9a4f1182dc98ff
unknown <andrew.raduege@gmail.com>
2018-05-13 15:50:02 -0500

    adding game manager to create multiple robots

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject gRobot;
+    public int numElements;
+    public float radius;
+
+	// Use this for initialization
+	void Start () {
+        float step = (2 * Mathf.PI) / numElements;
+        float angle = 0;
+
+        for(int i =0; i < numElements; i++)
+        {
+            float x = radius * Mathf.Cos(angle);
+            float y = radius * Mathf.Sin(angle);
+
+            GameObject newObject = (GameObject)Instantiate(gRobot, new Vector3(x, y, 0), Quaternion.identity);
+
+            angle += step;
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
