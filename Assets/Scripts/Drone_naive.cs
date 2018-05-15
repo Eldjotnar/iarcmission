8f7394fc4c4938dc9187285752c3e92a6dfe01d4
Magnus Cardell <cardell.magnus@gmail.com>
2018-05-15 10:03:05 -0500

    drone logic beta

---
+++ Assets/Scripts/Drone_naive.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+public class Drone_naive : MonoBehaviour {
+	//string mode = "naive";
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var foundObjects = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name == "gRobot(Clone)");
+		if(foundObjects.Count() > 0){
+			foreach(GameObject b in foundObjects){
+				Debug.Log(b + " : " + b.transform.position);
+			}
+		}
+	}
+}
