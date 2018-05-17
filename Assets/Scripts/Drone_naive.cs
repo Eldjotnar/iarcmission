654d29d02c2a096c1d658800d4c3e55b8c705a11
Magnus Cardell <cardell.magnus@gmail.com>
2018-05-17 16:58:24 -0500

    partial prio queue

--- Assets/Scripts/Drone_naive.cs
+++ Assets/Scripts/Drone_naive.cs
@@ -9,48 +9,85 @@ public class Drone_naive : MonoBehaviour {
 	private Transform target;
 	private LineRenderer line;
 	//private Collider droneCollider;
+
+	public Color c1 = Color.yellow;
+    public Color c2 = Color.red;
+    public int lengthOfLineRenderer = 10;
+
+	List<GameObject> roombas;
 	// Use this for initialization
 	void Start () {
+		LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        lineRenderer.widthMultiplier = 0.02f;
+        lineRenderer.positionCount = lengthOfLineRenderer;
 		
-		line = gameObject.AddComponent<LineRenderer>();
-		line.SetWidth(0.05F, 0.05F);
-		line.SetVertexCount(2);
-		//droneCollider = GetComponent<Collider>();
-		//droneCollider.enabled = false
+        // A simple 2 color gradient with a fixed alpha of 1.0f.
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+            );
+        lineRenderer.colorGradient = gradient;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 		findAll();
-		findBot("gBot0");
-		if(target){
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position -transform.position), 
-				speed * Time.deltaTime);
-			transform.position += transform.forward * speed * Time.deltaTime;
-		}
-        tapBot();
+
 	}
 
-	void findBot(string b){
+	void tapFirst(string b){
 		target = GameObject.Find(b).transform;
+		if(target){
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position -transform.position), 
+					speed * Time.deltaTime);
+				transform.position += transform.forward * speed * Time.deltaTime;
+			tapBot();
+		}
 	}
 
     void tapBot() {
-        findBot("gBot0");
         var at = target.GetComponent<botAttributes>();
         at.tapMe();
     }
 
 //Not used right now...
 	void findAll(){
-		var allRoombas = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name.Contains("gBot"));
-		if(allRoombas.Count() > 0){
-			foreach(GameObject b in allRoombas){
-				line.SetPosition(0, this.transform.position);
-				line.SetPosition(1, b.transform.position);
-				//Debug.Log(b + " : " + b.transform.position + "--> " + b.name);
+		var all = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => obj.name.Contains("gBot")).ToList();
+		var sorted = prioritize(all);
+		//var sorted = all.OrderBy(go=>go.transform.position.y).ToList();
+		List<GameObject> temp = sorted;
+		temp.Insert(0, this.gameObject);
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		//lineRenderer.SetPosition(0, transform.position);
+        for (int i = 0, i_end=temp.Count(); i < i_end; i++) {
+            lineRenderer.SetPosition(i, temp.ElementAt(i).transform.position);
+        }
+		tapFirst(temp.ElementAt(0).name);
+	}
+
+	List<GameObject> prioritize(List<GameObject> temp){
+		//prioritize according to bot closest to green line
+		List<GameObject> sorted= null;
+		sorted.Insert(0, temp[0]);
+		for(int i=1, i_end=temp.Count(); i<i_end; ++i){
+			float from = temp[i].transform.position.y;
+			float to = 4.7f;
+			float diff1 = to - from;
+
+			for(int j=0; j<i; ++j){
+				from = sorted[j].transform.position.y;
+				to = 4.7f;
+				float diff2 = to - from;
+				if(diff1 < diff2){
+					sorted.Insert(j, temp[i]);
+					break;
+				}
 			}
 		}
+		return sorted;
+		//roombas = sorted;
 	}
 }
