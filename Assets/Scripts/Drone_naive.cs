a703e7e74b292693afae3790eff4be81471e8358
Andreas Raduege <radueg1@stolaf.edu>
2018-05-18 00:43:20 -0500

    fixing circles?

--- Assets/Scripts/Drone_naive.cs
+++ Assets/Scripts/Drone_naive.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,23 +15,23 @@ public class Drone_naive : MonoBehaviour {
 	//private Collider droneCollider;
 
 	public Color c1 = Color.yellow;
-    public Color c2 = Color.red;
+	public Color c2 = Color.red;
 
 	List<GameObject> roombas; //retired
 	// Use this for initialization
 	void Start () {
 		LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
-        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
-        lineRenderer.widthMultiplier = 0.02f;
-		
-        // A simple 2 color gradient with a fixed alpha of 1.0f.
-        float alpha = 1.0f;
-        Gradient gradient = new Gradient();
-        gradient.SetKeys(
-            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
-            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
-            );
-        lineRenderer.colorGradient = gradient;
+		lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+		lineRenderer.widthMultiplier = 0.02f;
+
+		// A simple 2 color gradient with a fixed alpha of 1.0f.
+		float alpha = 1.0f;
+		Gradient gradient = new Gradient();
+		gradient.SetKeys(
+		new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+		new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+		);
+		lineRenderer.colorGradient = gradient;
 		InvokeRepeating("findNext", 1.0f, 5.0f);
 
 		StartCoroutine(directBot());
@@ -52,7 +52,7 @@ public class Drone_naive : MonoBehaviour {
 			if(diff.magnitude > 0.01) {
 				diff.Normalize();
 				float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-	      transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+				transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
 			}
 			transform.position += transform.up * speed * Time.deltaTime;
 		}
@@ -62,26 +62,26 @@ public class Drone_naive : MonoBehaviour {
 	}
 
 	IEnumerator directBot() {
-        float botHeading=0;
+		float botHeading=0;
 		while(true) {
 			yield return new WaitForSeconds(6);
 			print("checking directions");
-            try{
-			    botHeading = target.transform.eulerAngles.z;
-            }
-            catch(MissingReferenceException){
-                findBot("gBot2");
-            }
+			try{
+				botHeading = target.transform.eulerAngles.z;
+			}
+			catch(MissingReferenceException){
+				findBot("gBot2");
+			}
 			gBotController cs = target.GetComponent<gBotController>();
 			bool spinning = cs.spinning;
 			//Debug.Log(spinning);
 
 			if(botHeading > 180 && !spinning) {
 				print("Tapping!");
-				target.SendMessage("spinRobot", 180);
+				target.SendMessage("spinRobotFromDrone", 180);
 			} else if(botHeading > 90 && botHeading < 180) { // check if bot is in 2nd quadrant
 				print("Light tapping");
-				target.SendMessage("spinRobot", 45);
+				target.SendMessage("spinRobotFromDrone", 45);
 			}
 		}
 	}
@@ -93,7 +93,7 @@ public class Drone_naive : MonoBehaviour {
 	void findBot(string b) {
 		target = GameObject.Find(b);
 	}
-	
+
 	void greedySort(ref List<GameObject> bots){
 		prioritize(ref bots);
 		//bots = bots.OrderBy(go=>go.transform.position.y).ToList();
@@ -106,9 +106,9 @@ public class Drone_naive : MonoBehaviour {
 		LineRenderer lineRenderer = GetComponent<LineRenderer>();
 		lineRenderer.positionCount = temp.Count();
 		//lineRenderer.SetPosition(0, transform.position);
-        for (int i = 0, i_end=temp.Count(); i < i_end; i++) {
-            lineRenderer.SetPosition(i, temp.ElementAt(i).transform.position);
-        }	
+		for (int i = 0, i_end=temp.Count(); i < i_end; i++) {
+			lineRenderer.SetPosition(i, temp.ElementAt(i).transform.position);
+		}
 	}
 
 	void prioritize(ref List<GameObject> temp){
